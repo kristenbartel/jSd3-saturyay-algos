08721d77bcd4bf2bd9ea64d566bfe67a1f405440
Kristen Bartel <kristenbartel@gmail.com>
2021-12-19 15:30:31 -0600

    added bizzfuzz

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,14 +1,23 @@
+
 //fizzbuzz
-function fizzBuzz (num)
-let n = 1;
-while (n <= num) {
-        
-        if (n % 3 === 0) {
-                console.log('fizz');    
-        } else if (n % 5 === 0){
-                console.log('buzz');     
-        } else {
-                console.log(n);     
+
+
+// bizzfuzz for a range of nums
+let bizzfuzz = (num, num2) => {
+        while (num <= num2) {
+            
+                
+            if (num % 3 === 0 && num % 5 === 0) {
+                console.log("bizzfuzz"); 
+            } else if (num % 3 === 0){
+                        console.log('fizz');    
+                } else if (num % 5 === 0) {
+                        console.log('buzz');   
+                } else {
+                    console.log(num);
+                }
+                num++
         }
-        n++
-}
+    }
+
+    bizzfuzz(21, 51);
